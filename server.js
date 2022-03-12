f936b53c54ffc1977ff2e708201ca14876c0227a
Dawid Paszko <dawid.paszko@gmail.com>
2022-03-12 21:00:01 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import {WebSocketServer} from 'ws';
+
+const wss = new WebSocketServer({port:8080});
+wss.on('connection', client => {
+  client.on('message', (message,isBinary) => {
+    [...wss.clients]
+      .filter(c => c !== client)
+      .forEach(c => c.send(isBinary ? message.toString() : message));
+  });
+});
